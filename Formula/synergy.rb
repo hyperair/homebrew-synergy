b947becedb6da158fc035b5495b21c35f1ef30ee
Chow Loong Jin <hyperair@debian.org>
2017-04-24 14:12:26 +0800

    Update synergy to 1.8.7

--- Formula/synergy.rb
+++ Formula/synergy.rb
@@ -1,16 +1,16 @@
 class Synergy < Formula
   homepage "http://synergy-project.org"
-  url "https://github.com/symless/synergy/archive/1.6.3-final.tar.gz"
-  version "1.6.3-final"
+  url "https://github.com/symless/synergy/archive/v1.8.7-final.tar.gz"
+  version "1.8.7"
   sha256 "9740cb12e118c30ce94d4f5029d738198b31fec1814038c8631a7a031dba735d"
 
   depends_on "cmake" => :build
   depends_on "qt5" => :build
 
-  patch do
-    url "https://github.com/hyperair/synergy/compare/1.6.3-final...1.6-osx-fixups.patch"
-    sha256 "e52630fb4151bf9adf4a238efcaba4263fc883352e535ca5f40c976c08cf588d"
-  end
+  # patch do
+  #   url "https://github.com/hyperair/synergy/compare/1.6.3-final...1.6-osx-fixups.patch"
+  #   sha256 "e52630fb4151bf9adf4a238efcaba4263fc883352e535ca5f40c976c08cf588d"
+  # end
 
   def install
     system "./hm.sh", "conf", "-g1", "--mac-sdk", "#{MacOS.version}", "--mac-identity", "test"
